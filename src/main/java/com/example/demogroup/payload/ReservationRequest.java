4c4470c6a4cbcd528a41df3ae38271b790be6778
Angelos Kovatsis <95103346+Aggkov@users.noreply.github.com>
2022-08-05 04:01:54 +0300

    Delete src directory

--- src/main/java/com/example/demogroup/payload/ReservationRequest.java
+++
@@ -1,8 +0,0 @@
-package com.example.demogroup.payload;
-
-public class ReservationRequest {
-
-    private UserDto user;
-
-    private TimeslotResponse timeslot;
-}
--- src/main/java/com/example/demogroup/repository/VaccinationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demogroup.repository;
-
-import com.example.demogroup.model.Vaccination;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VaccinationRepository extends JpaRepository<Vaccination,Integer> {
-}
